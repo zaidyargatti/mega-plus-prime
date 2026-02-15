9a5f1c173ae2ac2c96d3e986a45143dddc982ae2
zaid Yargatti <9928zaid@gmail.com>
2026-02-15 15:01:14 +0530

    backend deploy url added

--- frontend/src/api/Axios.tsx
+++ frontend/src/api/Axios.tsx
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+
+const url1 ="https://mega-plus-prime.onrender.com" 
+const url2 ="http://localhost:3000/api"
 export default axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: url1 ?? url2
 });
+ 
